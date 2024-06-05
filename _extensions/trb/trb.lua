702f4f46e649f983e3015acf202ef73884dd6332
Matt Bhagat-Conway <matt@indicatrix.org>
2024-06-05 14:51:02 -0400

    allow overriding word/table count

--- _extensions/trb/trb.lua
+++ _extensions/trb/trb.lua
@@ -1,47 +1,56 @@
-words = 0
-tables = 0
+nwords = 0
+ntables = 0
 
+-- based on https://github.com/pandoc/lua-filters/blob/master/wordcount/wordcount.lua
 countwords = {
     Str = function(el)
         -- we don't count a word if it's entirely punctuation:
         if el.text:match("%P") then
-            words = words + 1
+            nwords = nwords + 1
         end
     end,
 
     Code = function(el)
         _,n = el.text:gsub("%S+","")
-        words = words + n
+        nwords = nwords + n
     end,
 
     CodeBlock = function(el)
         _,n = el.text:gsub("%S+","")
-        words = words + n
+        nwords = nwords + n
     end
 }
 
 trbfilter = {
     Table = function (el)
         -- if it's a table, just count it as a table
-        tables = tables + 1
+        ntables = ntables + 1
     end,
 
     Block = function (el)
         -- everything else gets counted
-        quarto.log.output(getmetatable(el))
-        -- otherwise, count words
         pandoc.walk_block(el, countwords)
     end,
 
     Meta = function(meta)
-        quarto.log.output("Words " .. words)
-        meta.nwords = words
+        if meta.nwords ~= nil then
+            nwords = tonumber(pandoc.utils.stringify(meta.nwords))
+        end
+
+        if meta.ntables ~= nil then
+            ntables = tonumber(pandoc.utils.stringify(meta.ntables))
+        end
+
+        quarto.log.output("Words " .. nwords)
+        quarto.log.output("Tables " .. ntables)
+
+        meta.nwords = nwords
         if tables == 1 then
             meta.ntables = "1 table"
         else
-            meta.ntables = tables .. " tables"
+            meta.ntables = tostring(ntables) .. " tables"
         end
-        meta.nwords_with_tables = words + tables * 250
+        meta.nwords_with_tables = nwords + ntables * 250
         return meta
     end
 }
